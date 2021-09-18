c0a5fa43b1747ea8fcfd4de06d1d197c5daaeca9
Seongheum Choi <kingman330@gmail.com>
2021-09-18 11:42:36 +0000

    IMPROVE beak 3009 solution

--- beak3009/src/main.rs
+++ beak3009/src/main.rs
@@ -1,7 +1,8 @@
 use std::io;
 use std::io::Read;
 use std::str::from_utf8;
-
+// exclusive OR 을 활용한 문제 풀이
+// 이전 풀이 너무 바보같다 ㅎ
 fn main() {
     let mut input: Vec<u8> = Vec::new();
     io::stdin().read_to_end(&mut input).unwrap();
@@ -9,35 +10,35 @@ fn main() {
         .split('\n').map(|x| x.split_whitespace()
             .map(|y| y.parse::<i32>().unwrap()).collect())
             .collect();
-    let mut x = 0;
-    let mut y = 0;
-
-    if input[0][0] == input[1][0] && input[0][1] == input[2][1] {
-        x = input[2][0];
-        y = input[1][1];
-    }
-    if input[0][0] == input[2][0] && input[0][1] == input[1][1] {
-        x = input[1][0];
-        y = input[2][1];
-    }
-
-
-    if input[1][0] == input[0][0] && input[1][1] == input[2][1] {
-        x = input[2][0];
-        y = input[0][1];
-    }
-    if input[1][0] == input[2][0] && input[1][1] == input[0][1] {
-        x = input[0][0];
-        y = input[2][1];
-    }
-
-    if input[2][0] == input[1][0] && input[2][1] == input[0][1] {
-        x = input[0][0];
-        y = input[1][1];
-    }
-    if input[2][0] == input[0][0] && input[2][1] == input[1][1] {
-        x = input[1][0];
-        y = input[0][1];
-    }
+    let x = input[0][0] ^ input[1][0] ^ input[2][0];
+    let y = input[0][1] ^ input[0][1] ^ input[0][1];
     println!("{} {}", x, y);
 }
+    // if input[0][0] == input[1][0] && input[0][1] == input[2][1] {
+    //     x = input[2][0];
+    //     y = input[1][1];
+    // }
+    // if input[0][0] == input[2][0] && input[0][1] == input[1][1] {
+    //     x = input[1][0];
+    //     y = input[2][1];
+    // }
+
+
+    // if input[1][0] == input[0][0] && input[1][1] == input[2][1] {
+    //     x = input[2][0];
+    //     y = input[0][1];
+    // }
+    // if input[1][0] == input[2][0] && input[1][1] == input[0][1] {
+    //     x = input[0][0];
+    //     y = input[2][1];
+    // }
+
+    // if input[2][0] == input[1][0] && input[2][1] == input[0][1] {
+    //     x = input[0][0];
+    //     y = input[1][1];
+    // }
+    // if input[2][0] == input[0][0] && input[2][1] == input[1][1] {
+    //     x = input[1][0];
+    //     y = input[0][1];
+    // }
+
