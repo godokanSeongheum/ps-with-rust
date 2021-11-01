1aba79713ca32f27ee6c78bbe63ba8cbeac20f39
nailerHeum <kingman330@gmail.com>
2021-11-01 13:56:22 +0000

    SOLVED beakjoon 9461 파도반 수열
      - 단순 println과 bufwriter의 차이가 크다

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,51 @@
-// beakjoon 9184 신나는 함수 실행
+// beakjoon 9461 파도반 수열
 use std::{
-    io::Read,
+    io::{self, Read, Write, BufWriter},
     str::from_utf8,
+    os::unix::io::FromRawFd,
+    fs::File,
     collections::HashMap
 };
 
-type TestCase = (isize, isize, isize);
-
-fn resolve_testcase(testcase: &[isize]) -> TestCase {
-    (testcase[0], testcase[1], testcase[2])
-}
-
-fn create_key(testcase: TestCase) -> String {
-    format!("{} {} {}", testcase.0, testcase.1, testcase.2)
-}
-
-fn get_answer(testcase: TestCase, memo: &mut HashMap<String, isize>) -> isize {
-    let key = create_key(testcase);
-    match memo.get(&key) {
-        Some(&n) => n,
+fn get_answer(n: isize, memo: &mut HashMap<isize, isize>) -> isize {
+    match memo.get(&n) {
+        Some(&answer) => answer,
         None => {
-            let answer = solve(testcase, memo);
-            memo.insert(key, answer);
+            let answer = solve(n, memo);
+            memo.insert(n, answer);
             answer
         },
     }
 }
 
-fn solve(testcase: TestCase, memo: &mut HashMap<String, isize>) -> isize {
-    let (a, b, c) = testcase;
-    if a <= 0 || b <= 0 || c <= 0 {
-        return 1;
+fn solve(n: isize, memo: &mut HashMap<isize, isize>) -> isize {
+    if n > 0 && n < 4 {
+        1
+    } else if n == 4 {
+        2
+    } else if n <= 0 {
+        0
+    } else {
+        get_answer(n - 1, memo) + get_answer(n - 5, memo)
     }
-    if a > 20 || b > 20 || c > 20 {
-        return get_answer((20, 20, 20), memo);
-    }
-    if a < b && b < c{
-        return get_answer((a, b, c - 1), memo) + 
-            get_answer((a, b - 1, c - 1), memo) -
-            get_answer((a, b - 1, c), memo);
-    } 
-    return get_answer((a - 1, b, c), memo) + 
-        get_answer((a - 1, b - 1, c), memo) +
-        get_answer((a - 1, b, c - 1), memo) -
-        get_answer((a - 1, b - 1, c - 1), memo);
 }
 fn main() {
-    use std::io;
+    let mut writer = BufWriter::new(unsafe {File::from_raw_fd(1)});
     let inputs: Vec<isize> = {
         let mut input = Vec::new();
         io::stdin().read_to_end(&mut input).unwrap();
         from_utf8(&input).unwrap().trim().split_whitespace()
+            .skip(1)
             .map(|x| x.parse().unwrap()).collect()
     };
-    let mut memo: HashMap<String, isize> = HashMap::new();
-    for testcase in inputs[..inputs.len() - 3].chunks(3) {
-        let (a, b, c) = resolve_testcase(&testcase);
-        let answer = match memo.get(create_key((a, b, c)).as_str()) {
+    let mut output = Vec::new();
+    let mut memo: HashMap<isize, isize> = HashMap::new();
+    for n in inputs.iter() {
+        let answer = match memo.get(n) {
             Some(&n) => n,
-            None => solve((a, b, c), &mut memo),
+            None => solve(*n, &mut memo),
         };
-        println!("w({}, {}, {}) = {}", a, b, c, answer);
+        write!(output, "{}\n", answer).unwrap();
     }
+    writer.write_all(&output).unwrap();
 }
