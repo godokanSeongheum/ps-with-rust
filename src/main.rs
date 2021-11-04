86ee3f1dae719c4f1bbe305e6753ea0056c84a2d
nailerHeum <kingman330@gmail.com>
2021-11-04 04:54:46 +0000

    SOLVED beakjoon 2579 계단 오르기

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// beakjoon 1932 정수 삼각형
+// beakjoon 2579 계단 오르기
 use std::{
     io::{self, Read, Write, BufWriter},
     str::from_utf8,
@@ -7,28 +7,31 @@ use std::{
 };
 fn main() {
     let mut writer = BufWriter::new(unsafe {File::from_raw_fd(1)});
-    let inputs: Vec<Vec<usize>> = {
+    let inputs: Vec<usize> = {
         let mut input = Vec::new();
         io::stdin().read_to_end(&mut input).unwrap();
-        from_utf8(&input).unwrap().trim().split("\n")
+        from_utf8(&input).unwrap().trim().split_whitespace()
             .skip(1)
-            .map(|x| {
-                x.trim().split_whitespace()
-                    .map(|y| y.parse().unwrap())
-                    .collect()
-            })
+            .map(|x| x.parse().unwrap())
             .collect()
     };
+    let mut memo: [(usize, usize);300] = [(0, 0);300];
     let mut output = Vec::new();
-    let mut arr: [[usize;500];500] = [[10000;500];500];
-    for (idx, &val) in inputs[inputs.len() - 1].iter().enumerate() {
-        arr[0][idx] = val;
-    }
-    for (idx,line) in inputs.iter().rev().skip(1).enumerate() {
-        for (i, &v) in line.iter().enumerate() {
-            arr[idx + 1][i] = arr[idx][i].max(arr[idx][i + 1]) + v;
+    for (idx, &num) in inputs.iter().enumerate() {
+        if idx == 0 {
+            memo[0] = (num, 0);
+            continue;
         }
+        if idx == 1 {
+            memo[1] = (memo[0].0 + num, num);
+            continue;
+        }
+        memo[idx] = (
+            memo[idx - 1].1 + num,
+            memo[idx - 2].0.max(memo[idx - 2].1) + num
+        );
     }
-    write!(output, "{}", arr[inputs.len() - 1][0]).unwrap();
+    let result = memo[inputs.len() - 1].0.max(memo[inputs.len() - 1].1);
+    write!(output, "{}", result).unwrap();
     writer.write_all(&output).unwrap();
 }
