7c76c41e70a70f9677f8ee30861e5103e26877a4
Seongheum Choi <kingman330@gmail.com>
2021-11-05 14:28:44 +0000

    SOLVED beakjoon 10844 쉬운 계단 수
      - 큰수 나머지 문제가 생소해서 잡는데 시간이 좀 걸렸다.
      - 점화식을 찾는데 집중하기

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// beakjoon 1463 1로 만들기
+// beakjoon 10844 쉬운 계단 수
 use std::{
     io::{self, Write, BufWriter},
     os::unix::io::FromRawFd,
@@ -6,31 +6,22 @@ use std::{
 };
 fn main() {
     let mut writer = BufWriter::new(unsafe {File::from_raw_fd(1)});
-    let x: usize = {
+    let n: usize = {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        input.trim().parse().unwrap()
+        input.trim().parse::<usize>().unwrap() - 1
     };
     let mut output = Vec::new();
-    let mut table: [usize;1_000_001] = [0;1_000_001];
-    table[2] = 1;
-    table[3] = 1;
-    for i in 4..1_000_001 {
-        if i % 2 == 0 && i % 3 == 0 {
-            table[i] = table[i / 2].min(table[i / 3]).min(table[i - 1]) + 1;
-            continue;
+    let mut table: [[u64;10];100] = [[0;10];100]; 
+    table[0] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    for i in 1..=n {
+        table[i][0] = table[i - 1][1];
+        table[i][9] = table[i - 1][8];
+        for digit in 1..9 {
+            table[i][digit] = (table[i - 1][digit - 1] + table[i - 1][digit + 1]) %
+                                                1_000_000_000;
         }
-        if i % 3 == 0 {
-            table[i] = table[i / 3].min(table[i - 1]) + 1;
-            continue;
-        }
-        if i % 2 == 0 {
-            table[i] = table[i / 2].min(table[i - 1]) + 1;
-            continue;
-        }
-        table[i] = table[i - 1] + 1;
     }
-
-    write!(output, "{:?}", table[x]).unwrap();
+    write!(output, "{}", table[n].iter().sum::<u64>() % 1_000_000_000).unwrap();
     writer.write_all(&output).unwrap();
 }
