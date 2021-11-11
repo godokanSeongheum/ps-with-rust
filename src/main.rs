1ab77ba9a5a9c1f1aa92a56d9932cffb5108a73d
Seongheum Choi <kingman330@gmail.com>
2021-11-11 14:16:43 +0000

    SOLVED baekjoon 12865 평범한 배낭
      - knapsack problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// baekjoon 2565 전깃줄
+// baekjoon 12865 평범한 배낭
 use std::{fs::File, io::{self, Read, Write, BufWriter}, os::unix::io::FromRawFd, str::from_utf8};
 fn main() {
     let mut writer = BufWriter::new(unsafe {File::from_raw_fd(1)});
@@ -6,29 +6,29 @@ fn main() {
         let mut input = Vec::new();
         io::stdin().read_to_end(&mut input).unwrap();
         from_utf8(&input).unwrap().trim().split_whitespace()
-            .skip(1).map(|x| x.parse().unwrap())
+            .map(|x| x.parse().unwrap())
             .collect()
     };
-    let mut output = Vec::new();
-
-    let mut arr: [usize;501] = [0;501];
-    for slice in inputs.chunks(2) {
-        arr[slice[0]] = slice[1];
+    let mut items: [(usize, usize);101] = [(0, 0);101];
+    let k = inputs[1];
+    for (i, chunk) in inputs.chunks(2).skip(1).enumerate() {
+        items[i].0 = chunk[0]; items[i].1 = chunk[1];
     }
-    let arr: Vec<usize> = arr.iter()
-        .filter(|&x| *x != 0).map(|x| *x).collect();
-    let mut memo: Vec<usize> = vec![0;arr.len()];
-    for i in 0..arr.len() {
-        let mut max_val = 0;
-        for j in 0..i {
-            if arr[i] > arr[j] && memo[j] > max_val {
-                max_val = memo[j];
+    items.sort_by_key(|(w, _)| *w);
+
+    let mut dp: Vec<[usize;100_001]> = vec![[0;100_001];items.len() + 1];
+    for (i, item) in items.iter().enumerate() {
+        for w in 1..=k {
+            dp[i + 1][w] = dp[i][w];
+            if w >= item.0 {
+                dp[i + 1][w] = dp[i + 1][w].max(dp[i][w - item.0] + item.1);
             }
         }
-        memo[i] = max_val + 1;
     }
 
-    let result: usize = arr.len() - *(memo.iter().max().unwrap());
+
+    let mut output = Vec::new();
+    let result = dp[items.len()][k];
     write!(output, "{}", result).unwrap();
     writer.write_all(&output).unwrap();
 }
