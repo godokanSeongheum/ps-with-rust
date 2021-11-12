0ca1c1fed2c9125b346a29b802c669f69989f4c2
Seongheum Choi <kingman330@gmail.com>
2021-11-12 11:49:46 +0000

    SOLVED baekjoon 1931 회의실 배정
      - rust sort_by를 사용한 편리하고 효율적인 sorting을 익히자

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
-// baekjoon 11047 동전 0
+// baekjoon 1931 회의실 배정
 use std::{fs::File, io::{self, Read, Write, BufWriter}, os::unix::io::FromRawFd, str::from_utf8};
 fn main() {
     let mut writer = BufWriter::new(unsafe {File::from_raw_fd(1)});
-    let inputs: Vec<usize>  = {
+    let mut inputs: Vec<(usize, usize)>  = {
         let mut input = Vec::new();
         io::stdin().read_to_end(&mut input).unwrap();
-        from_utf8(&input).unwrap().trim().split_whitespace()
-            .map(|x| x.parse().unwrap())
+        from_utf8(&input).unwrap().trim().split("\n")
+            .skip(1).map(|x| {
+                let mut line = x.split_whitespace()
+                    .map(|y| y.parse().unwrap());
+                (line.next().unwrap(), line.next().unwrap())
+            })
             .collect()
     };
     let mut output = Vec::new();
-    let n = inputs[0];
-    let mut k = inputs[1];
+    inputs.sort_by_key(|x| x.1);
+    inputs.sort_by_key(|x| x.0);
     let mut count = 0;
-    for i in (2..n + 2).rev() {
-        let div = k / inputs[i];
-        if div != 0 {
-            count += div;
-            k %= inputs[i]
+    let mut prev = (0, 0);
+    for i in 0..inputs.len() {
+        if prev.1 <= inputs[i].0 {   // safe
+            count += 1;
+            prev = inputs[i];
+        } else {
+            if prev.1 >= inputs[i].1 {
+                prev = inputs[i];
+            }
         }
     }
-    
     let result = count;
     write!(output, "{}", result).unwrap();
     writer.write_all(&output).unwrap();
 }
+
+// inputs[i]를 평가
