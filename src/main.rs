f831d1207ee367d605eeea2bf4a1376bd691a4e4
Seongheum Choi <kingman330@gmail.com>
2021-11-12 10:55:12 +0000

    SOLVED baekjoon 11047 동전 0
      - greedy 첫 시작

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// baekjoon 12865 평범한 배낭
+// baekjoon 11047 동전 0
 use std::{fs::File, io::{self, Read, Write, BufWriter}, os::unix::io::FromRawFd, str::from_utf8};
 fn main() {
     let mut writer = BufWriter::new(unsafe {File::from_raw_fd(1)});
@@ -9,26 +9,19 @@ fn main() {
             .map(|x| x.parse().unwrap())
             .collect()
     };
-    let mut items: [(usize, usize);101] = [(0, 0);101];
-    let k = inputs[1];
-    for (i, chunk) in inputs.chunks(2).skip(1).enumerate() {
-        items[i].0 = chunk[0]; items[i].1 = chunk[1];
-    }
-    items.sort_by_key(|(w, _)| *w);
-
-    let mut dp: Vec<[usize;100_001]> = vec![[0;100_001];items.len() + 1];
-    for (i, item) in items.iter().enumerate() {
-        for w in 1..=k {
-            dp[i + 1][w] = dp[i][w];
-            if w >= item.0 {
-                dp[i + 1][w] = dp[i + 1][w].max(dp[i][w - item.0] + item.1);
-            }
+    let mut output = Vec::new();
+    let n = inputs[0];
+    let mut k = inputs[1];
+    let mut count = 0;
+    for i in (2..n + 2).rev() {
+        let div = k / inputs[i];
+        if div != 0 {
+            count += div;
+            k %= inputs[i]
         }
     }
-
-
-    let mut output = Vec::new();
-    let result = dp[items.len()][k];
+    
+    let result = count;
     write!(output, "{}", result).unwrap();
     writer.write_all(&output).unwrap();
 }
