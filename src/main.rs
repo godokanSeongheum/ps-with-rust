4f4d83169b93501bf5024e631df8450c6499af21
Seongheum Choi <kingman330@gmail.com>
2021-11-13 14:32:23 +0000

    SOLVED baekjoon 5086 배수와 약수

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-// baekjoon 13305 주유소
+// baekjoon 5086 배수와 약수
 use std::io;
 fn main() {
-    let mut n = String::new();
-    let mut distances = String::new();
-    let mut prices = String::new();
-    let reader  = io::stdin();
-    reader.read_line(&mut n).unwrap();
-    reader.read_line(&mut distances).unwrap();
-    reader.read_line(&mut prices).unwrap();
-    let distances: Vec<u64> = distances.trim().split_whitespace()
-        .map(|x| x.parse().unwrap()).collect();
-    let prices: Vec<u64> = prices.trim().split_whitespace()
-        .map(|x| x.parse().unwrap()).collect();
-    
-    let mut budget: u64 = 0;
-    let mut minimum_price: u64 = u64::MAX;
-    for i in 0..distances.len() {
-        minimum_price = minimum_price.min(prices[i]);
-        budget += minimum_price * distances[i];
+    let stdin = io::stdin();
+    let mut line = String::new();
+    loop {
+        stdin.read_line(&mut line).unwrap();
+        let mut iter = line.trim().split_whitespace()
+            .map(|x| x.parse::<usize>().unwrap());
+        if let Some(a) = iter.next() {
+            if let Some(b) = iter.next() {
+                if a == 0 && b == 0 {
+                    break;
+                }
+                if b % a == 0 {
+                    println!("factor");
+                } else if a % b == 0 {
+                    println!("multiple");
+                } else {
+                    println!("neither");
+                }
+            }
+        } else {
+            break;
+        }
+        line.clear();
     }
-    println!("{}", budget);
 }
