d18e51b6a2e4b4d7b8a86c6d797f2abad66c009c
Seongheum Choi <kingman330@gmail.com>
2021-11-14 05:33:01 +0000

    SOLVED baekjoon 2609 최대공약수와 최소공배수

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
-// baekjoon 1037 약수
+// baekjoon 2609 최대공약수와 최소공배수
 use std::io;
 fn main() {
     let stdin = io::stdin();
-    let mut n = String::new();
-    let mut nums = String::new();
-    stdin.read_line(&mut n).unwrap();
-    stdin.read_line(&mut nums).unwrap();
-    let nums: Vec<usize> = nums.trim().split_whitespace()
-        .map(|x| x.parse().unwrap()).collect();
-    let mut min_val = usize::MAX;
-    let mut max_val = usize::MIN;
-    for num in nums {
-        if num < min_val {
-            min_val = num;
-        }
-        if num > max_val {
-            max_val = num;
+    let mut ab = String::new();
+    stdin.read_line(&mut ab).unwrap();
+    let mut iter = ab.trim().split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap());
+    let first = iter.next().unwrap();
+    let second = iter.next().unwrap();
+    let mut a = first;
+    let mut b = second;
+    let mut gcd: usize = usize::MAX;
+    loop {
+        if a < b {
+            if b % a == 0 {
+                gcd = a;
+                break;
+            } else {
+                b = b % a;
+            }
+        } else {
+            if a % b == 0 {
+                gcd = b;
+                break;
+            } else {
+                a = a % b;
+            }
         }
     }
-    println!("{}", min_val * max_val);
+    println!("{}\n{}", gcd, first * second / gcd);
 }
